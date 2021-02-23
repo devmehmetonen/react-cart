c9337c0190cbbfe9777121512bce786574e0f779
Mehmet Onen <mehmet.onen.job@gmail.com>
2021-02-23 08:56:25 +0300

    react-shoppin-app

---
+++ src/components/Cart.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react'
+import formatCurrency from '../util';
+import Fade from 'react-reveal/Fade';
+
+export default class Cart extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            email: "",
+            address: "",
+            showCheckOut: false
+        }
+    }
+
+    handleInput = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    createOrder = (e) => {
+        e.preventDefault();
+        const order = {
+            name: this.state.name,
+            email: this.state.email,
+            address: this.state.address,
+            cartItems: this.props.cartItems
+        }
+
+        this.props.createOrder(order);
+    }
+
+    render() {
+        const { cartItems } = this.props;
+        return (
+            <div>
+                {cartItems.length === 0 ? (<div className="cart cart-header">Cart is empty</div>) :
+                    (<div className="cart cart-header">You have {cartItems.length} in the cart</div>)
+                }
+                <div>
+                    <div className="cart">
+                        <Fade left cascade>
+                            <ul className="cart-items">
+                                {cartItems.map(ci => (
+                                    <li key={ci._id}>
+                                        <div>
+                                            <img src={ci.image} alt={ci.title}></img>
+                                        </div>
+                                        <div>
+                                            <div>
+                                                {ci.title}
+                                            </div>
+                                            <div className="right">
+                                                {formatCurrency(ci.price)} x {ci.count}{" "}
+                                                <button onClick={() => this.props.removeFromCart(ci)}>
+                                                    Remove
+                                            </button>
+                                            </div>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        </Fade>
+                    </div>
+                    {cartItems.length !== 0 && (
+                        <Fade bottom cascade>
+                            <div>
+                                <div className="cart">
+                                    <div className="total">
+                                        <div>
+                                            Total:{" "}
+                                            {formatCurrency(cartItems.reduce((a, c) => a + c.price * c.count, 0))}
+                                        </div>
+                                        <button className="button primary"
+                                            onClick={() => { this.setState({ showCheckOut: true }) }}
+                                        >Proceed</button>
+                                    </div>
+                                </div>
+                                {this.state.showCheckOut && (
+                                    <Fade right cascade>
+                                        <div className="cart">
+                                            <form onSubmit={this.createOrder}>
+                                                <ul className="form-container">
+                                                    <li>
+                                                        <label>Email:</label>
+                                                        <input name="email" type="email" required onChange={this.handleInput}></input>
+                                                    </li>
+                                                    <li>
+                                                        <label>Name:</label>
+                                                        <input name="name" type="text" required onChange={this.handleInput}></input>
+                                                    </li>
+                                                    <li>
+                                                        <label>Address:</label>
+                                                        <input name="address" type="text" required onChange={this.handleInput}></input>
+                                                    </li>
+                                                    <li>
+                                                        <button type="submit" className="button primary">Checkout</button>
+                                                    </li>
+                                                </ul>
+                                            </form>
+                                        </div>
+                                    </Fade>
+                                )}
+                            </div>
+                        </Fade>
+                    )}
+                </div>
+            </div>
+        )
+    }
+}
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,32 +1,98 @@
 import React, { Component } from 'react';
-import formatCurrency from '../util'
+import formatCurrency from '../util';
+import Fade from 'react-reveal/Fade';
+import Zoom from 'react-reveal/Zoom';
+import Modal from 'react-modal';
 
 export default class Products extends Component {
+
+    constructor(props){
+        super(props);
+        this.state ={
+            product:null
+        }
+    }
+
+    openModal = (product) =>{
+        this.setState({product})
+    }
+
+    closeModal = () =>{
+        this.setState({product:null})
+    }
+
     render() {
+        const {product} = this.state;
         return (
             <div>
-                <ul className="products">
-                    {this.props.products.map(p=>(
-                       <li key={p._id}>
-                           <div className="product">
-                                <a href={"#" + p._id}>
-                                  <img src={p.image} alt={p.title}></img>
-                                  <p>
-                                      {p.title}
-                                  </p>
-                                </a>
-                                <div className="product-price">
-                                    <div>
-                                        {formatCurrency(p.price)}
-                                    </div>
-                                    <button className="button primary">
-                                        Add To Cart
+                <Fade bottom cascade={true}>
+                    <ul className="products">
+                        {this.props.products.map(p => (
+                            <li key={p._id}>
+                                <div className="product">
+                                    <a href={"#" + p._id} onClick={()=>this.openModal(p)}>
+                                        <img src={p.image} alt={p.title}></img>
+                                        <p>
+                                            {p.title}
+                                        </p>
+                                    </a>
+                                    <div className="product-price">
+                                        <div>
+                                            {formatCurrency(p.price)}
+                                        </div>
+                                        <button className="button primary"
+                                            onClick={() => this.props.addToCart(p)}>
+                                            Add To Cart
                                     </button>
+                                    </div>
                                 </div>
-                           </div>
-                       </li>
-                    ))}
-                </ul>
+                            </li>
+                        ))}
+                    </ul>
+                </Fade>
+                {
+                    product && (
+                        <Modal isOpen={true}>
+                            <Zoom>
+                                <button className="close-modal" onClick={this.closeModal}>x</button>
+                                <div className="product-details">
+                                    <img src={product.image} alt={product.title}></img>
+                                    <div className="product-details-description">
+                                        <p>
+                                            <strong>{product.title}</strong>
+                                        </p>
+                                        <p>
+                                            {product.description}
+                                        </p>
+                                        <p>
+                                            Avaiable Sizes:{" "}
+                                            {product.availableSizes.map((a)=>(
+                                                <span>{" "}
+                                                    <button className="button">
+                                                        {a}
+                                                    </button>
+                                                </span>
+                                            ))}
+                                        </p>
+                                        <div className="product-price">
+                                            <div>
+                                                {
+                                                   formatCurrency(product.price)
+                                                }
+                                            </div>
+                                            <button className="button primary"
+                                                    onClick={()=>{
+                                                        this.props.addToCart(product);
+                                                        this.closeModal();
+                                                    }}
+                                            >Add To Cart</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Zoom>
+                        </Modal>
+                    )
+                }
             </div>
         )
     }
